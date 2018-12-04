d71a2bae260ac6f97343268ae4b77b790c505651
c811k <bhk811@gmail.com>
2018-12-04 04:30:52 -0800

    working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,110 +1,131 @@
 var trivia = [
     {question: "Which of the following sports is not part of the triathlon?", 
     choices:["Cycling", "Swimming", "Running", "Horse-Riding"],
-    answer: "Horse-Riding"},
+    answer: "Horse-Riding",
+    image: "assets/images/horse-riding.jpg"},
     {question: "Which country will host the 2020 Summer Olympics?", 
     choices:["China", "Australia", "Germany", "Japan"],
-    answer: "Japan"},
+    answer: "Japan",
+    image: "assets/images/japan.jpg"},
     {question: "Which of the following pitchers was named National League Rookie of the Year for the 2013 season?", 
     choices:["Jose Fernandez", "Jacob deGrom", "Shelby Miller", "Matt Harvey"],
-    answer: "Jose Fernandez"},
+    answer: "Jose Fernandez",
+    image: "assets/images/jose-fernandez.jpg"},
     {question: "Which team has won the most Stanley Cups in the NHL?",
     choices:["Chicago Blackhawks", "Toronto Maple Leafs", "Montreal Canadians", "Detroit Red Wings"],
-    answer: "Montreal Candadians"},
+    answer: "Montreal Canadians",
+    image: "assets/images/montreal-canadians.jpg"},
     {question: "What was the final score of the Germany vs. Brazil 2014 FIFA World Cup match?", 
     choices:["0 - 1", "7 - 1", "3 - 4", "16 - 0"],
-    answer: "7 - 1"}
+    answer: "7 - 1",
+    image: "assets/images/germany.jpg"},
 ];
-console.log(trivia);
 
 var questionIndex = 0;
-var score = 0;
-var breakTime = 3;
-var time = 31;
+var correct = 0;
+var incorrect = 0;
+var count = 30;
 var intervalId;
 
-$("#question-area").hide();
 //Click the button to start the game
-$("#start").on("click", startGame)
+$("#start").on("click", next);
+
+$("#questions").on("click", "button", function(key) {
+    var userGuess = $(this).text();
+    if(userGuess === trivia[questionIndex].answer) {
+        clearInterval(clock);
+        win();
+    }
+    else {
+        clearInterval(clock);
+        loss();
+    }
+});
 
 function displayQuestion() {
+    $("#questions").html("<h2>" + 
+        trivia[questionIndex].question + 
+    	"</h2><button class= 'choices btn btn-lg btn-outline-secondary btn-block mt-5'>" + 
+    	trivia[questionIndex].choices[0] + 
+    	"<button class= 'choices btn btn-lg btn-outline-secondary btn-block'>" + 
+    	trivia[questionIndex].choices[1] + 
+    	"<button class= 'choices btn btn-lg btn-outline-secondary btn-block'>" +  
+    	trivia[questionIndex].choices[2] + 
+    	"<button class= 'choices btn btn-lg btn-outline-secondary btn-block'>" + 
+    	trivia[questionIndex].choices[3]);
+}
     
-    console.log(trivia);
-    $("#questions").html("<h2>" + trivia[questionIndex].question + "</h2>");
+function win() {
+    $("#questions").html("<h2>Correct!</h2>");
+    $("#questions").append("<img src=" + trivia[questionIndex].image + " class='img-thumbnail'>");
+    correct++;
+	setTimeout(next, 4000);
+	questionIndex++;
+}
 
-    for (var i = 0; i < 4; i++) {
-        var button = $("<button class= 'btn btn-lg btn-outline-secondary btn-block'>");
-        button.text(trivia[questionIndex].choices[i]);
-        button.attr('data-id', i);
-        $("#choices").append(button);
+function loss() {
+    $("#questions").html("<h2>You are wrong!</h2>");
+    var correctAnswer = trivia[questionIndex].answer;
+    $("#questions").append("<h4>The answer was " + correctAnswer + "</h4></class>");
+    $("#questions").append("<img src=" + trivia[questionIndex].image + " class='img-thumbnail'>");
+    incorrect++;
+    setTimeout(next, 4000);
+    questionIndex++;
+}
+
+function userTimeout() {
+    if(count === 0) {
+        $("#questions").html("<h2>Time Up!</h2>");
+        var correctAnswer = trivia[questionIndex].answer;
+        console.log(correctAnswer);
+        $("#questions").append("<h4>The answer was " + correctAnswer + "</h4>");
+        $("#questions").append("<img src=" + trivia[questionIndex].image + " class='img-thumbnail'>");
+        incorrect++;
+        setTimeout(next, 4000);
+        questionIndex++;
     }
-    result();
-
-    intervalId = setInterval(timer, 1000);
 }
 
 function result() {
-    $("#choices").on("click", "button", function(key) {
-        var userChoice = $(this).trivia[questionIndex].answer;
-        console.log(userChoice);
-        console.log(trivia[questionIndex].answer);
-        clearInterval(intervalId);
-        time = 31;
-
-        if(userChoice !== trivia[questionIndex].answer) {
-            $("#choices").html("Wrong Answer!");
-            $("#choices").html("Correct answer is " + trivia[questionIndex].answer);
-        } 
-        else if (userChoice === trivia[questionIndex].answer) {
-            $("#choices").html("Correct!");
-        }
-        intervalId = setInterval(bTime, 1000);
-        
-    });
-}
-
-function updateScore() {
-    $("#score").text("Score:" + score);
-    
-}
-
-function startGame() {
-    timer();
-    displayQuestion();
-    $("#start").hide();
-    $("#question-area").show();
-        
-}
-function bTime() {
-    breakTime--;
-
-    if(breakTime === 0) {
-        clearInterval(intervalId);
-        $("#choices").empty();
-        displayQuestion();
-        timer();
-    }
+   
+    $("#questions").html("<h3>Correct: " + correct + "</h3>" + "<h3>Incorrect: " + incorrect + "</h3>");
+    $("#questions").append("<button id='start-over' type='button' class='btn btn-lg btn-outline-secondary btn-block mt-5'>Start Over?</button>");
+    $("#start-over").on("click", next);
+    reset();
 }
 
 function timer() {
-    time--;
-
-    $("#time").html("<h2>Time Remaining: " + time + " Seconds</h2>");
-    if (time === 0) {
-        clearInterval(intervalId);
-        $("#choices").html("Times Up!");
-
-        return;
+    clock = setInterval(countdown, 1000);
+    function countdown() {
+        if(count < 1) {
+            clearInterval(clock);
+            userTimeout();
+        }
+        if(count > 0) {
+            count--;
+        }
+        $("#time").html("<h2>Time Remaining: " + count + " Seconds</h2>");
     }
 }
-function next() {
-    questionIndex++;
-        
-    setTimeout(displayQuestion, 1000);
-      
-    
-    // if(count === images.length) {
-    // //     count = 0;
-    // // }
 
+function next() {
+    $("#start").hide();
+    if (questionIndex < trivia.length) {
+        count = 30;
+        $("#time").html("<h2>Time Remaining: " + count + " Seconds</h2>");
+        displayQuestion();
+        timer();
+        userTimeout();
+    }
+    else {
+        result();
+    }
+    
 }
+
+function reset() {
+    questionIndex = 0;
+    correct = 0;
+    incorrect = 0;
+}
+
